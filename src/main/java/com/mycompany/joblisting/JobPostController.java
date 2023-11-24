2617898408b90b091a18782a0b0fc961090eca4e
Vijay Saddi <vijaysaddi@Vijays-MacBook-Pro.local>
2023-11-23 21:45:01 -0500

    create initial post controller for swagger

--- src/main/java/com/mycompany/joblisting/JobPostController.java
+++ src/main/java/com/mycompany/joblisting/JobPostController.java
@@ -1,4 +1,16 @@
 package com.mycompany.joblisting;
 
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
 public class JobPostController {
+
+    @RequestMapping(value = "/")
+    public void redirect(HttpServletResponse response) throws IOException {
+        response.sendRedirect("/swagger-ui.html");
+    }
 }
